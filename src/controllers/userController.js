3bac9a0be571d509dc35860e47acf7c9b14974ab
kjy5657 <45618920+kjy5657@users.noreply.github.com>
2022-01-24 20:49:29 +0900

    Socal Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
@@ -48,6 +49,13 @@ export const postLogin = async (req, res) => {
     });
   }
 
+  if (user.socialOnly === true) {
+    return res.status(400).render("login", {
+      pageTitle: "login",
+      errorMessage: "Login with Github!",
+    });
+  }
+
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
     return res.status(400).render("login", {
@@ -64,9 +72,92 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  //encoding params
+  const params = new URLSearchParams(config).toString();
+  const url = `https://github.com/login/oauth/authorize?${params}`;
+  return res.redirect(url);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const url = `https://github.com/login/oauth/access_token?${params}`;
+
+  const tokenRequest = await (
+    await fetch(url, {
+      method: "POST",
+      headers: { Accept: "application/json" },
+    })
+  ).json();
+
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    //public data
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    //private data (if email is private data)
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+
+    //github의 email과 같은 email이 이미 유저 DB에 존재한다면..?
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      //해당 email로 없으니까, create an account
+      user = await User.create({
+        //if no password, have to login with social
+        name: userData.name,
+        avatarUrl: userData.avatar_url,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      //verified email, get login
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) => res.send("Log out");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -8,12 +9,12 @@ import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
-
 app.use(
   session({
-    secret: "hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 app.use(localsMiddleware);
